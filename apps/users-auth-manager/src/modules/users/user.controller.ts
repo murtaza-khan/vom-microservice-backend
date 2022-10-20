ac8a8e67ef1846f7923b9aa673346a1b19f444cc
Muhammad Murtaza <muhammad.murtaza@retailo.co>
2022-10-20 23:05:30 +0500

    resolve confilict

--- apps/users-auth-manager/src/modules/users/user.controller.ts
+++ apps/users-auth-manager/src/modules/users/user.controller.ts
@@ -1,23 +1,21 @@
-
-import {
-  Controller,
-  Get,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { CsvParser } from 'nest-csv-parser';
 import { UserService } from './user.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Readable } from 'stream';
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, Query, UploadedFile, UseInterceptors } from "@nestjs/common";
-import { FileInterceptor } from "@nestjs/platform-express";
-import { diskStorage } from "multer";
-import { CsvParser } from "nest-csv-parser";
-import { UserService } from "./user.service";
-import { Readable } from 'stream'
-import { sign , verify } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { GraphqlAuthGuard } from '../auth/guards/gql-auth.guard';
@@ -35,7 +33,6 @@ export class UserController {
     private readonly userService: UserService,
     private readonly csvParser: CsvParser
   ) {}
-
   @GrpcMethod('UsersService', 'findById')
   async findById({ id }): Promise<any> {
     console.log('*******************', id);
@@ -74,46 +71,35 @@ export class UserController {
     }
   }
 
-@Controller('user')
-export class UserController {
-    constructor(private readonly userService: UserService,
-        private readonly csvParser: CsvParser) { }
-    @UseGuards(GraphqlAuthGuard)
-    @UseGuards(RolesGuard)
-    @Roles(UserRoles.ADMIN, UserRoles.GROUP_MANAGER)
-    @Post('/createUsersFromCSV')
-    @UseInterceptors(FileInterceptor('file'))
-    async uploadFile(@UploadedFile() file: Express.Multer.File, @CurrentUserController() currentUser: any) {
-        try {
-            const csvStream = Readable.from(file.buffer);
-            const entities = await this.csvParser.parse(csvStream, Entity, null, null, { separator: ',' });
-            return this.userService.createUserFromCSV(entities.list, currentUser.organization, currentUser.groupId);
-        } catch (error) {
-            throw Error(error);
-        }
+  @Post('/forgotPassword')
+  async forgotPassword(@Body() body: any) {
+    return await this.userService.forgotPassword(body.email);
+  }
+
+  @Get('/reset-password/:id/:token')
+  async resetPassword(@Param('id') id: any, @Param('token') token: any) {
+    return await this.userService.resetPassword(id, token);
+  }
+
+  @Post('/reset-password/:id/:token')
+  async resetPasswordUpdate(
+    @Param('id') id: any,
+    @Param('token') token: any,
+    @Body() body: any
+  ) {
+    if (body.confirmPassword != body.newPassword) {
+      throw new HttpException(
+        'Confirm Password is not matched',
+        HttpStatus.BAD_REQUEST
+      );
+    } else {
+      return await this.userService.resetPasswordUpdate(
+        id,
+        token,
+        body.newPassword
+      );
     }
-
-
-    @Post('/forgotPassword')
-    async forgotPassword(@Body() body: any){
-       return await this.userService.forgotPassword(body.email);
-    }
-
-    @Get('/reset-password/:id/:token')
-    async resetPassword(@Param('id') id: any, @Param('token') token: any) {
-       return await this.userService.resetPassword(id , token);
-    }
-
-    @Post('/reset-password/:id/:token')
-    async resetPasswordUpdate(@Param('id') id: any, @Param('token') token: any, @Body() body: any) {
-        if(body.confirmPassword != body.newPassword){
-            throw new HttpException('Confirm Password is not matched' , HttpStatus.BAD_REQUEST);
-        }
-        else{
-            return await this.userService.resetPasswordUpdate(id , token , body.newPassword);
-        }
-    }
-
+  }
 }
 
 class Entity {
